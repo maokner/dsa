048ed3a341e30578d0cfbeedc22812ba5a954b24
Matt Okner <115849717+maokner@users.noreply.github.com>
2026-09-07 23:04:24 -0700

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        letters = {}
+        for c in s:
+            if c in letters:
+                letters[c] += 1
+            else:
+                letters[c] = 1
+        for c in t:
+            if c in letters:
+                letters[c] -= 1
+                if letters[c] == 0:
+                    del letters[c]
+            else:
+                return False
+        return len(letters.keys()) == 0
+        
